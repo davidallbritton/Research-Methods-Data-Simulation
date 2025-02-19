f3fdeb49081cd331a248eba9ebd6a58dab702464
david <dallbrit@depaul.edu>
2025-02-19 14:48:31 -0800

    incremental but has a wrong extra column

--- dataSimulationWithScales/tempfile.R
+++ dataSimulationWithScales/tempfile.R
@@ -1,38 +1,38 @@
-generate_likert_data <- function(N, scaleLength, likertMin, likertMax) {
+generate_likert_data <- function(N, scaleLength, likertMin, likertMax, ivName = "A") {
   # Create a dataframe with N*2 rows
   df3 <- data.frame(
-    A = rep("", N * 2)  # Placeholder for A values
+    ivName = rep("", N * 2)  # Placeholder for A values
   )
   
   # Generate scale items (a1 to a[scaleLength])
   for (i in 1:scaleLength) {
-    df3[[paste0("A_", i)]] <- sample(likertMin:likertMax, size = N * 2, replace = TRUE)
+    df3[[paste0(ivName, "_", i)]] <- sample(likertMin:likertMax, size = N * 2, replace = TRUE)
   }
   
   # Compute the mean of all scale items
-  df3$A_Mean <- rowMeans(df3[, paste0("A_", 1:scaleLength)])
+  df3[[paste0(ivName, "_Mean")]] <- rowMeans(df3[, paste0(ivName, "_", 1:scaleLength)])
   
   # Sort by A_Mean
-  df3 <- df3[order(df3$A_Mean, decreasing = TRUE), ]
+  df3 <- df3[order(df3[[paste0(ivName, "_Mean")]], decreasing = TRUE), ]
   
   # Assign A values based on ranking
-  df3$A <- rep(c("A1", "A2"), each = N)
+  df3[[ivName]] <- rep(c(paste0(ivName, "1"), paste0(ivName, "2")), each = N)
   
   # Identify duplicate A_Mean values between A1 and A2
-  duplicate_means <- intersect(df3$A_Mean[df3$A == "A1"], df3$A_Mean[df3$A == "A2"])
+  duplicate_means <- intersect(df3[[paste0(ivName, "_Mean")]][df3[[ivName]] == paste0(ivName, "1")], df3[[paste0(ivName, "_Mean")]][df3[[ivName]] == paste0(ivName, "2")])
   
   if (length(duplicate_means) > 0) {
     for (mean_value in duplicate_means) {
-      affected_rows <- which(df3$A == "A1" & df3$A_Mean == mean_value)
+      affected_rows <- which(df3[[ivName]] == paste0(ivName, "1") & df3[[paste0(ivName, "_Mean")]] == mean_value)
       for (row in affected_rows) {
         # Find the column with the minimum integer value
-        min_col <- which.min(df3[row, paste0("A_", 1:scaleLength)])
+        min_col <- which.min(df3[row, paste0(ivName, "_", 1:scaleLength)])
         # Increment that value by 1
-        df3[row, paste0("A_", min_col)] <- df3[row, paste0("A_", min_col)] + 1
+        df3[row, paste0(ivName, "_", min_col)] <- df3[row, paste0(ivName, "_", min_col)] + 1
       }
     }
     # Recalculate the means
-    df3$A_Mean <- rowMeans(df3[, paste0("A_", 1:scaleLength)])
+    df3[[paste0(ivName, "_Mean")]] <- rowMeans(df3[, paste0(ivName, "_", 1:scaleLength)])
   }
   
   return(df3)
